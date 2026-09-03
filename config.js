c3a839b58cd3ebd137fe43ab9d584f2c213f6499
RIAN-AKBAR <rian032008@gmail.com>
2026-09-03 10:12:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CDP_CONFIG = {
     // ============================================
     // URL TUNNEL CLOUDFLARE - GANTI DI SINI SAJA
     // ============================================
-    TUNNEL_URL: 'https://logs-tension-chuck-lately.trycloudflare.com',
+    TUNNEL_URL: 'https://clients-jeff-arguments-offices.trycloudflare.com',
     
     // ============================================
     // PATH REGISTER & ADMIN
