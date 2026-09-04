82c6cd670e0d3d0d3643acbd55b2a507d5c0bd70
RIAN-AKBAR <rian032008@gmail.com>
2026-09-04 18:08:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const CDP_CONFIG = {
     // ============================================
     EXTERNAL_LINKS: {
         ROLXUM: 'https://rolxum-store.vercel.app/',
-        CINE_UMBU: 'https://cine-umbu-store.vercel.app',
+        CINE_UMBU: 'https://cine-by-umbu.ai.studio',
         JASA_CINE: 'https://jasacinembyanzarcine.netlify.app/',
         TIKTOK: 'https://tiktok.com/@umbu2460',
         DISCORD: 'https://discord.gg/kEYVZJGdk',
