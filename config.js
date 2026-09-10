8d5de538dc76923a987f4d4ae4ce4df08bc385e4
RIAN-AKBAR <rian032008@gmail.com>
2026-09-10 14:07:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CDP_CONFIG = {
     // ============================================
     // URL TUNNEL CLOUDFLARE - GANTI DI SINI SAJA
     // ============================================
-    TUNNEL_URL: 'https://casting-reason-competitors-coupled.trycloudflare.com',
+    TUNNEL_URL: 'https://definition-addition-backgrounds-adapted.trycloudflare.com',
     
     // ============================================
     // PATH REGISTER & ADMIN
