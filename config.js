ac6ae374278517389e1e254c6ad1dfe6a8bd85f7
RIAN-AKBAR <rian032008@gmail.com>
2026-09-13 11:31:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CDP_CONFIG = {
     // ============================================
     // URL TUNNEL CLOUDFLARE - GANTI DI SINI SAJA
     // ============================================
-    TUNNEL_URL: 'https://triumph-sophisticated-involvement-mrna.trycloudflare.com',
+    TUNNEL_URL: 'https://beaches-adequate-maui-afternoon.trycloudflare.com',
     
     // ============================================
     // PATH REGISTER & ADMIN
