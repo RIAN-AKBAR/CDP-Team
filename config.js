3b5b89d42c03bbc7dd35a347aa61ac3ad1d4ea35
RIAN-AKBAR <rian032008@gmail.com>
2026-09-17 07:39:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CDP_CONFIG = {
     // ============================================
     // URL TUNNEL CLOUDFLARE - GANTI DI SINI SAJA
     // ============================================
-    TUNNEL_URL: 'https://beaches-adequate-maui-afternoon.trycloudflare.com',
+    TUNNEL_URL: 'https://hardware-gpl-paragraphs-prisoner.trycloudflare.com',
     
     // ============================================
     // PATH REGISTER, ADMIN & CHAT
